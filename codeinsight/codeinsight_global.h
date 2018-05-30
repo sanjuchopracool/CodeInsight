6e97344359e1759ab7ef269950057829783664d8
Sanjay <sanjuchopracool@gmail.com>
2018-05-31 01:26:11 +0530

    Updated files

--- codeinsight/codeinsight_global.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <QtGlobal>
-
-#if defined(CODEINSIGHT_LIBRARY)
-#  define CODEINSIGHTSHARED_EXPORT Q_DECL_EXPORT
-#else
-#  define CODEINSIGHTSHARED_EXPORT Q_DECL_IMPORT
-#endif
