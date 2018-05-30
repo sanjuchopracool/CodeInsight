f53141da823f053d7c261e1f0598ba52d82f8516
Sanjay Chopra <sanjuchopracool@gmail.com>
2018-05-30 14:16:36 +0530

    added QtCreator as submodule

---
+++ codeinsight/codeinsightconstants.h
@@ -0,0 +1,13 @@
+#pragma once
+
+namespace CodeInsight {
+namespace Constants {
+
+const char ACTION_ID[] = "codeinsight.Action";
+const char MENU_ID[] = "codeinsight.Menu";
+
+const char CPPEDITOR_ID[] = "CodeInsight.C++Editor";
+const char CPPEDITOR_DISPLAY_NAME[] = QT_TRANSLATE_NOOP("OpenWith::Editors", "Code Insight C++ Editor");
+
+} // namespace codeinsight
+} // namespace Constants
